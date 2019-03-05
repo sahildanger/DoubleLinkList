14a4734a4c34e2262f7de360f56e2478502d3aa9
sahildanger <sahil.chauhan_ccv17@gmail.com>
2019-03-05 14:55:11 +0530

    Initial commit

---
+++ src/main/DoubleLinkList.java
@@ -0,0 +1,74 @@
+package main;
+class Node{
+    int data;
+    Node next;
+    Node previous;
+}
+
+public class DoubleLinkList {
+Node head;
+int size;
+
+    public static void main(String[] args) {
+        DoubleLinkList obj=new DoubleLinkList();
+        for (int i = 0; i < 5; i++) {
+            Node node=new Node();
+            node.data=(i+1)*100;
+            obj.insert(node);
+        }
+        obj.display();
+        obj.delete();
+    }
+    void insert(Node node){
+        System.out.println("inserting node"+node.data+"at last");
+        if(head==null){
+            head=node;
+        }
+        else
+        {
+            Node temp=head;
+            while (temp.next!=null){
+                temp=temp.next;
+            }
+            node.previous=temp;
+            temp.next=node;
+        }
+    }
+    void display(){
+        System.out.println("Doubly LinkedList");
+        Node temp=head;
+        System.out.println("null<-->");
+        while (temp!=null){
+            System.out.println(temp.data+"<--->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    void delete(){
+        if(head==null){
+            System.out.println("empty list");
+            }
+        else
+        {
+            Node temp=head;
+            while (temp!=null){
+                temp=temp.next;
+            }
+            System.out.println("deleting node"+temp.data+"from last");
+            temp=temp.previous;
+            temp.next=null;
+        }
+    }
+    boolean search(Node node){
+        boolean response=false;
+        Node temp=head;
+        while (temp!=null){
+            if(temp.data==node.data){
+                response=true;
+                break;
+            }
+            temp=temp.next;
+        }
+        return response;
+    }
+}
