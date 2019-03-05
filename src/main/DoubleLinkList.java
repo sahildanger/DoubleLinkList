6942af7028dca74e12c7b9f808561fd7291f76e7
sahildanger <sahil.chauhan_ccv17@gmail.com>
2019-03-05 15:11:04 +0530

    2

--- src/main/DoubleLinkList.java
+++ src/main/DoubleLinkList.java
@@ -1,74 +1,93 @@
 package main;
-class Node{
+
+class Node {
     int data;
     Node next;
     Node previous;
 }
 
 public class DoubleLinkList {
-Node head;
-int size;
+    Node head;
+    int size;
 
     public static void main(String[] args) {
-        DoubleLinkList obj=new DoubleLinkList();
+        DoubleLinkList obj = new DoubleLinkList();
         for (int i = 0; i < 5; i++) {
-            Node node=new Node();
-            node.data=(i+1)*100;
+            Node node = new Node();
+            node.data = (i + 1) * 100;
             obj.insert(node);
         }
         obj.display();
         obj.delete();
     }
-    void insert(Node node){
-        System.out.println("inserting node"+node.data+"at last");
-        if(head==null){
-            head=node;
-        }
-        else
-        {
-            Node temp=head;
-            while (temp.next!=null){
-                temp=temp.next;
+
+    void insert(Node node) {
+        System.out.println("inserting node" + node.data + "at last");
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-            node.previous=temp;
-            temp.next=node;
+            node.previous = temp;
+            temp.next = node;
         }
     }
-    void display(){
+
+    void display() {
         System.out.println("Doubly LinkedList");
-        Node temp=head;
+        Node temp = head;
         System.out.println("null<-->");
-        while (temp!=null){
-            System.out.println(temp.data+"<--->");
-            temp=temp.next;
+        while (temp != null) {
+            System.out.println(temp.data + "<--->");
+            temp = temp.next;
         }
         System.out.println("null");
     }
-    void delete(){
-        if(head==null){
+
+    void delete() {
+        if (head == null) {
             System.out.println("empty list");
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-        else
-        {
-            Node temp=head;
-            while (temp!=null){
-                temp=temp.next;
-            }
-            System.out.println("deleting node"+temp.data+"from last");
-            temp=temp.previous;
-            temp.next=null;
+            System.out.println("deleting node" + temp.data + "from last");
+            temp = temp.previous;
+            temp.next = null;
         }
     }
-    boolean search(Node node){
-        boolean response=false;
-        Node temp=head;
-        while (temp!=null){
-            if(temp.data==node.data){
-                response=true;
+
+    boolean search(Node node) {
+        boolean response = false;
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == node.data) {
+                response = true;
                 break;
             }
-            temp=temp.next;
+            temp = temp.next;
         }
         return response;
+    }
+
+    void insertAtBeginning(Node node) {
+        System.out.println("inserting node" + node.data + "at beginning");
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head.previous = node;
+            head = node;
+        }
+        size++;
+    }
+
+    void deletingFromBeginning() {
+        System.out.println("deleting node" + head.data + "from beginning");
+        head = head.next;
+        head.previous = null;
     }
 }
