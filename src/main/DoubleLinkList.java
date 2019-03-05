27d38a745e2b8cbb4ad1fb5d7abc6e930561ab6b
sahildanger <sahil.chauhan_ccv17@gmail.com>
2019-03-05 15:15:55 +0530

    3

--- src/main/DoubleLinkList.java
+++ src/main/DoubleLinkList.java
@@ -90,4 +90,18 @@ public class DoubleLinkList {
         head = head.next;
         head.previous = null;
     }
+    void insertBeforeNode(Node node,Node nodebefore){
+        System.out.println("inserting node"+node.data+"before node"+nodebefore.data);
+        Node temp=head;
+        while (temp!=null) {
+            if (temp.data == nodebefore.data) {
+                temp.previous.next = node;
+                node.next = temp;
+                node.previous = temp.previous;
+                temp.previous = node;
+                break;
+            }
+            temp = temp.next;
+        }
+    }
 }
